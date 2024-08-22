ec424b77c906e6f05ab876c1de66a50a55192b05
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-08-22 20:09:36 +0530

    ZTM 114,115 lessons consepts and one script to run both ends window powerchell confificaration in npm added and first getAllPlanets route added

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors')
+
+const planetsRouter = require('../src/routes/planets/planets.router');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 
+}));
 app.use(express.json());
+
+app.use(planetsRouter);
 
 module.exports = app;
