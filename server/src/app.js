c68b88fd0db5b3db3c6e3573b4a8a75f2e5fb240
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-08-25 20:50:25 +0530

    launch router added in backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('../src/routes/planets/planets.router');
+const launchesRouter = require('../src/routes/launches/launches.router');
 
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   optionsSuccessStatus: 200 
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"..","public")));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
+
+app.get("/",(req, res) => {
+  res.sendFile(path.join(__dirname,"..","public","index.html"));
+});
 
 module.exports = app;
