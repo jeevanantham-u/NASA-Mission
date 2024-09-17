59d33cbe3afde9ac70db6f747f14582c1e2eb731
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-09-17 23:11:53 +0530

    DB init mongo db atlas createed and mongoose connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,19 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
-const { loadPlanetsData } = require('./models/planets.model')
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = "mongodb+srv://jeeva:2jRXMp5G0OXTUm3c@nasacluster.kgjz8.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
 
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('error', (err) => console.error(err));
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
@@ -16,3 +22,5 @@ async function startServer() {
 }
 
 startServer();
+
+//2jRXMp5G0OXTUm3c
