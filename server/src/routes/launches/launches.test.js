0bb171b352478b6ffc67b1a6f9280e59015feff9
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-11-12 23:02:11 +0530

    ci error on test loadplanets module added and client test bypassed

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 const{
-  loadPlanetsData
+  loadPlanetsData,
 } = require('../../models/planets.model');
 
 describe('Launches API', () => {
